0660cfe295dd7494a58ef1304cb636095efb054b
emiyl <me@emiyl.com>
2025-05-08 15:28:01 +0100

    use vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import "@/styles/globals.scss";
 import '@/lib/fontawesome';
 import styles from "@/styles/layout.module.scss";
@@ -20,6 +21,7 @@ export default function RootLayout({
         <Navbar />
         <main className={styles.main}>
           {children}
+          <Analytics />
         </main>
       </body>
     </html>
