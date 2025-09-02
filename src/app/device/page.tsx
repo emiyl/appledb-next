6722adfaccd7d4a6a487316e6174b7d1d1690702
emiyl <me@emiyl.com>
2025-09-02 20:01:07 +0100

    use suspense for entrylist

--- src/app/device/page.tsx
+++ src/app/device/page.tsx
@@ -1,12 +1,15 @@
 import styles from '@/styles/layout.module.scss';
+import { Suspense } from 'react';
 import { EntryList } from '@/components/EntryList';
 import { EntryType } from '@/types';
 
 export default function DeviceEntryListPage() {
-  return (
-    <main className={styles.content}>
-      <h1>Device List</h1>
-      <EntryList entryType={EntryType.Device} />
-    </main>
-  );
+    return (
+        <main className={styles.content}>
+            <h1>Device List</h1>
+            <Suspense fallback={<div>Loading...</div>}>
+                <EntryList entryType={EntryType.Device} />
+            </Suspense>
+        </main>
+    );
 }
--- src/app/firmware/page.tsx
+++ src/app/firmware/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import { EntryList } from '@/components/EntryList';
 import { EntryType } from '@/types';
 import styles from '@/styles/layout.module.scss';
@@ -6,7 +7,9 @@ export default function OsEntryListPage() {
   return (
     <main className={styles.content}>
       <h1>Firmware Table</h1>
-      <EntryList entryType={EntryType.Os} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <EntryList entryType={EntryType.Os} />
+      </Suspense>
     </main>
   );
 }
