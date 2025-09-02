56aab9192211b7422835539dd98b0713f1f4a357
emiyl <me@emiyl.com>
2025-09-02 19:25:05 +0100

    let device entry list page take params from URL

--- src/app/device/page.tsx
+++ src/app/device/page.tsx
@@ -1,12 +1,32 @@
+'use client'
 import styles from '@/styles/layout.module.scss';
 import { EntryList } from '@/components/EntryList';
-import { EntryType } from '@/types';
+import { EntryType, DeviceEntryListFilter } from '@/types';
+import { defaultDeviceEntryListFilter } from '@/utils';
+import { useSearchParams } from 'next/navigation';
+import { obfuscateNumber, deobfuscateNumber } from '@/utils/obfuscate';
 
 export default function DeviceEntryListPage() {
+  const searchParams = useSearchParams();
+  const params = Object.fromEntries(searchParams.entries());
+
+  const defaultFilter = {
+    ...defaultDeviceEntryListFilter,
+    search: params.search || defaultDeviceEntryListFilter.search,
+    filter_id: params.category
+      ? params.category
+          .split(',')
+          .map((item) => {
+            const id = item/*.split('.').pop() ?? ''*/;
+            return deobfuscateNumber(parseInt(id));
+          })
+      : defaultDeviceEntryListFilter.filter_id,
+  };
+
   return (
     <main className={styles.content}>
       <h1>Device List</h1>
-      <EntryList entryType={EntryType.Device} />
+      <EntryList entryType={EntryType.Device} defaultFilter={defaultFilter} />
     </main>
   );
 }
